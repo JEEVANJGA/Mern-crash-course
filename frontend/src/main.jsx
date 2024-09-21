689bcd252074c2f27085b2479bd36606d6f78150
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-09-21 13:26:46 +0530

    Comment out StrictMode in main.jsx for simplified rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -6,11 +6,11 @@ import App from "./App.jsx";
 // import './index.css'
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+  // </StrictMode>
 );
